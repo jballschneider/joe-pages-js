427f1868b8a1fed722d06b17f179d6f677bef9c7
jballschneider <55411969+jballschneider@users.noreply.github.com>
2025-12-19 09:24:04 -0500

    Change entity type from 'locations' to 'location'

--- src/templates/locations.tsx
+++ src/templates/locations.tsx
@@ -48,7 +48,7 @@ export const config: TemplateConfig = {
     ],
     // Defines the scope of entities that qualify for this stream.
     filter: {
-      entityTypes: ["locations"],
+      entityTypes: ["location"],
     },
     // The entity language profiles that documents will be generated for.
     localization: {
